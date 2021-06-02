6cbb067a6ce5df24d814bb01ec84d712312cdff8
Max Dayton <daytonmaximus@gmail.com>
2021-06-02 01:35:41 -0600

    MDT ERC20 token contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,8 +12,15 @@ async function main() {
   const token = await Token.deploy();
 
   await token.deployed();
-
+  
   console.log("Token deployed to:", token.address);
+
+  const MDToken = await hre.ethers.getContractFactory("MDToken")
+  const mdToken = await MDToken.deploy("Maximus Dayton Token", "MDT")
+
+  await mdToken.deployed()
+
+  console.log("Maximus Dayton Token deployed to: ", mdToken.address)
 }
 
 main()
